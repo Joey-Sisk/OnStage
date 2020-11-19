5a85c2883f45750f649243718baa63d99b90d387
Joey Sisk <jjmsisk@gmail.com>
2020-11-18 21:16:15 -0600

    initial js

---
+++ script.js
@@ -0,0 +1,37 @@
+function buildQueryURL() {
+
+  const keyword = $("#searchBar").val().trim();
+
+  const queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&source=universe&countryCode=US&apikey=LEUwCGTcHM9FHCOKSOYvPObtTKbpoEax`;
+ 
+  console.log("---------------\nURL: " + queryURL + "\n---------------");
+
+  return queryURL;
+}
+
+function updatePage(eventData) {
+  console.log(eventData);
+  console.log("------------------------------------");
+
+  $("#place1").text();
+
+  $("#place2").text();
+
+  $("#place3").text();
+
+  $("#place4").text();
+
+}
+
+
+
+$("#searchButton").on("click", function(event) {
+  event.preventDefault();
+
+  var queryURL = buildQueryURL();
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(updatePage);
+});
