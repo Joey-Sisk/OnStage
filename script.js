5ee138c6c720c1bc46b2b18fef481cebdba421c3
Joey Sisk <jjmsisk@gmail.com>
2020-11-23 19:05:05 -0600

    fixed search box to search on submit

--- script.js
+++ script.js
@@ -56,13 +56,13 @@ function updatePage() {
       nov: "",
       dec: ""
     };
-    
+
     const chartBarText = "22%2C2%2C3%2C9%2C15%2C2%2C7%2C3%2C13%2C14%2C4";
     const chartBarSize = "22%7C2%7C3%7C9%7C15%7C2%7C7%7C3%7C13%7C14%7C4";
 
     const chartQueryURL = `https://image-charts.com/chart?chbr=8&chd=t:${chartBarText}&chl=${chartBarSize}&chma=0%2C0%2C10%2C10&chs=700x450&cht=bvs&chtt=Shows%20per%20month&chxl=0%3A%7CJan%7CFeb%7CMar%7CApr%7CMay%7CJun%7CJul%7CAug%7CSep%7CNov%7CDec&chxt=x%2Cy`;
 
-    
+
     console.log("---------------\nChart URL: " + chartQueryURL + "\n---------------");
 
     $.ajax({
@@ -73,13 +73,10 @@ function updatePage() {
       for (let i = 0; i < eventAmount; i++) {
         const eventMonth = eventData[i].datetime
         console.log("Every Event Month: " + eventMonth.slice(5, 7));
-        
+
       }
-
     });
-
   });
-
 }
 
 $("#runSearch").on("click", function (event) {
@@ -87,6 +84,10 @@ $("#runSearch").on("click", function (event) {
 
   updatePage();
 
+});
+
+$("#searchBer").submit(function(event){
+  event.preventDefault();
 });
 
 // 5e819cfc0dc5827e31d44c0ea761bf34
